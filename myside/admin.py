08a7cdba4e40ef494d9e084768b24143ece7972c
heosang <57525618+heosang@users.noreply.github.com>
2020-09-03 18:33:35 +0900

    myside.admin
    product 및 category admin 생성

--- myside/admin.py
+++ myside/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('first','second' )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
