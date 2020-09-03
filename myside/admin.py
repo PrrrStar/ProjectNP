481fc96b03fb4e9dac6ef314e155731aab90db35
heosang <57525618+heosang@users.noreply.github.com>
2020-09-03 22:56:11 +0900

    Update admin.py
    ProductAdmin,
    CategoryAdmin, CommentAdmin, RecommentAdmin, BrandAdmin,
    Product_has_brandAdmin
    admin 생성

--- myside/admin.py
+++ myside/admin.py
@@ -5,13 +5,24 @@ from .models import *
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', )
 
-
-
-
-
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('first','second' )
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display=('content',)
+class RecommentAdmin(admin.ModelAdmin):
+    list_display=('content',)
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display=('name',)
+
+class Product_has_brandAdmin(admin.ModelAdmin):
+    list_display=()
 
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Recomment, RecommentAdmin)
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Product_has_brand, Product_has_brandAdmin)
