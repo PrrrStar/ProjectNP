b6a091cbfaca6ced99baf526ba5e803e066345a8
Seo Jun Hyeok <46551990+taulmax@users.noreply.github.com>
2020-09-06 23:50:32 +0900

    index.html 수정, detail.html 생성
    DB를 잘 받아오는지 확인중입니다.

--- myside/views.py
+++ myside/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'myside/index.html')
+    products = Product.objects.all()
+    return render(request, 'myside/index.html', {'products': products})
