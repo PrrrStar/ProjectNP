bebe71b99eb55a02b7ca28b4e37da1b9e9b2c9c0
comye1 <50735594+comye1@users.noreply.github.com>
2020-09-07 00:30:08 +0900

    views.py에 product_detail 추가 (index to detail)

--- myside/views.py
+++ myside/views.py
@@ -7,3 +7,7 @@ from .models import *
 def index(request):
     products = Product.objects.all()
     return render(request, 'myside/index.html', {'products': products})
+
+def product_detail(request, id, product_slug=None):
+    product = get_object_or_404(Product, id=id, slug=product_slug)
+    return render(request, 'myside/detail.html', {'product': product})
