21406bdd51701b6fa756e28f89406a4e6468d6ae
heosang <57525618+heosang@users.noreply.github.com>
2020-09-08 00:35:14 +0900

    Update models.py

--- member/models.py
+++ member/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    phone = models.CharField(
+        max_length=100
+    )
+
+    class Meta:
+        db_table = 'account_profile'
+        app_label = 'account'  # <- account 앱 카테고리에서 관리되도록 한다.
