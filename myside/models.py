3e30706c08928bd422e23ff7319e06053aff6104
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-09-07 16:53:10 +0900

    delete product_has_brand model

--- myside/models.py
+++ myside/models.py
@@ -7,7 +7,7 @@ from mptt.models import MPTTModel, TreeForeignKey
 
 class Category(MPTTModel):
 
-    name    = models.CharField(max_length=20, verbose_name='카테고리', unique=True)
+    name    = models.CharField(max_length=20, verbose_name='카테고리', unique=True, db_index=True)
     parent  = TreeForeignKey('self', null=True, blank= True, verbose_name='상위 카테고리',related_name='children', on_delete=models.CASCADE)
     slug    = models.SlugField(max_length=20, db_index = True, allow_unicode=True)
 
@@ -36,27 +36,29 @@ class Category(MPTTModel):
 
 
 class Brand(models.Model):
-    name    = models.CharField(max_length=20, verbose_name='브랜드')
-    img     = models.ImageField(upload_to="img/brand", blank=True, null=True)
+    name    = models.CharField(max_length=20, verbose_name='브랜드', db_index=True)
+    slug    = models.SlugField(max_length= 20, db_index=True, allow_unicode=True)
+    img     = models.ImageField(upload_to="brand", blank=True, null=True)
+
+    class Meta:
+        ordering = ['-name']
+        index_together = [['id','slug']]
+        verbose_name        = 'brand'
+        verbose_name_plural = 'brands'
 
     def __str__(self):
         return self.name
 
-    class Meta:
-        verbose_name        = 'brand'
-        verbose_name_plural = 'brands'
-
 class Product(models.Model):
-    category            = TreeForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name = 'products')
-    brand               = models.ManyToManyField(Brand, through='Product_has_brand')
-
     name                = models.CharField(max_length = 20, db_index=True, verbose_name='제품명')
-    slug                = models.SlugField(max_length = 20, db_index=True, allow_unicode=True)
-    img                 = models.ImageField(upload_to="img/products/%Y/%m/%d", blank=True, null=True)
+    category            = TreeForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name = 'products')
+    brand               = models.ManyToManyField(Brand)
+    img                 = models.ImageField(upload_to="products/%Y/%m/%d", blank=True, null=True)
     description         = models.TextField(verbose_name='설명', blank=True)
     price               = models.DecimalField(verbose_name='가격', max_digits = 10, decimal_places=0)
     stock               = models.PositiveIntegerField(verbose_name='재고')
     available_display   = models.BooleanField('판매 가능?', default= True) 
+    slug                = models.SlugField(max_length = 20, db_index=True, allow_unicode=True)
 
     created_at          = models.DateTimeField(auto_now_add=True, verbose_name='등록날짜')
     modified_at         = models.DateTimeField(auto_now=True, verbose_name='수정날짜')
@@ -76,15 +78,9 @@ class Product(models.Model):
         return reverse('myside:product_detail', args=[self.id, self.slug])
 
 
-
-class Product_has_brand(models.Model):
-    product     = models.ForeignKey(Product, verbose_name="제품명", on_delete=models.SET_NULL, null=True)
-    brand       = models.ForeignKey(Brand, verbose_name="브랜드명", on_delete=models.SET_NULL, null=True)
-
-
 class Comment(models.Model):
     product     = models.ForeignKey(Product, verbose_name="제품명", on_delete=models.CASCADE)
-    img         = models.ImageField(upload_to="img/comments/%Y/%m/%d", blank=True)
+    img         = models.ImageField(upload_to="comments/%Y/%m/%d", blank=True)
     content     = models.TextField(verbose_name='내용')
     created_at  = models.DateTimeField(auto_now_add=True, verbose_name='등록날짜')
 
