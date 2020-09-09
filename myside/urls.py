1ffc0349c0e2c4b450da70c06044d7b892214948
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-09-10 00:29:49 +0900

    ready to convert DRF

--- myside/urls.py
+++ myside/urls.py
@@ -5,6 +5,6 @@ app_name = 'myside'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('detail/<int:id>/', product_detail, name='product_detail'),
-    path('category/<category_slug>/', product_in_category, name='product_in_category'),
+    path('products/<int:id>/', product_detail, name='product_detail'),
+    path('<category_slug>/', product_in_category, name='product_in_category'),
 ]
