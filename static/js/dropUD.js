b791c2de225c0dfdf75c74a169734bae396126c5
Seo Jun Hyeok <46551990+taulmax@users.noreply.github.com>
2020-09-10 21:34:56 +0900

    reset.css 추가 및 css 살짝 수정
    reset.css 추가 및 css 살짝 수정

--- static/js/dropUD.js
+++ static/js/dropUD.js
@@ -1,19 +1,19 @@
 const parents = document.querySelectorAll(".category__parent");
 const children = document.querySelectorAll(".category__children");
 
-function dropUD(event){
+function dropUD(event) {
   name = event.target.getAttribute("value");
-  if(event.target.getAttribute("drop") == "f"){
-    for (var i=0; i<children.length; i++){
-      if(name == children[i].getAttribute("value")){
+  if (event.target.getAttribute("drop") == "f") {
+    for (var i = 0; i < children.length; i++) {
+      if (name == children[i].getAttribute("value")) {
         children[i].style.display = "flex";
       }
     }
     event.target.setAttribute("drop", "t")
   }
-  else{
-    for (var i=0; i<children.length; i++){
-      if(name == children[i].getAttribute("value")){
+  else {
+    for (var i = 0; i < children.length; i++) {
+      if (name == children[i].getAttribute("value")) {
         children[i].style.display = "none";
       }
     }
@@ -21,6 +21,6 @@ function dropUD(event){
   }
 }
 
-for (var i=0; i<parents.length; i++){
+for (var i = 0; i < parents.length; i++) {
   parents[i].addEventListener("click", dropUD);
 }
