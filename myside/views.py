0ea5cfc1800026f2c232e4bc7ffe3479588defa8
Seo Jun Hyeok <46551990+taulmax@users.noreply.github.com>
2020-09-10 21:43:31 +0900

    상세 페이지에도 카테고리가 뜨게 했습니다.
    상세 페이지에도 카테고리가 뜨게 했습니다.

--- myside/views.py
+++ myside/views.py
@@ -12,17 +12,20 @@ def index(request):
 
 def product_detail(request, id, product_slug=None):
     product = get_object_or_404(Product, id=id)
-    return render(request, 'myside/detail.html', {'product': product})
+    categories = Category.objects.all()
+    return render(request, 'myside/detail.html', {'product': product, 'categories': categories})
 
-def product_in_category(request, category_slug = None):
+
+def product_in_category(request, category_slug=None):
     current_category = None
     products = Product.objects.filter(available_display=True)
 
     if category_slug:
         current_category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=current_category, available_display=True)
+        products = products.filter(
+            category=current_category, available_display=True)
 
     return render(request, 'myside/list.html', {
-                'current_category': current_category,
-                'products': products,
-                })
+        'current_category': current_category,
+        'products': products,
+    })
