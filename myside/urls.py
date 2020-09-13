c78b3eac74d8c5fcad5337ed96395651b2b1d964
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-09-13 14:48:11 +0900

    upload sample data (도시락)

--- myside/urls.py
+++ myside/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from .views import *
 
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
    path('', index, name='index'),
@@ -14,3 +16,6 @@ urlpatterns = [
     path('api/products/', ProductList.as_view(), name=ProductList.name),
     path('api/products/<int:pk>/', ProductDetail.as_view(), name=ProductDetail.name),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
