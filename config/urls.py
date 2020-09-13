88023af48a026b393daea5333a7699e844a78e9b
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-09-13 16:57:56 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('myside.urls')),
+    path('admin/', admin.site.urls),
     path('post/', include('post.urls')),
     path('accounts/', include('allauth.urls')),
 ]
