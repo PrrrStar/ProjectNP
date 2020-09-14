6b6d004f075024c3d5be6dcedb8584e14c5421e3
heosang <57525618+heosang@users.noreply.github.com>
2020-09-14 12:16:18 +0900

    Update urls.py
    member추가

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('post/', include('post.urls')),
     path('accounts/', include('allauth.urls')),
+    path('member/', include('member.urls')),
 ]
