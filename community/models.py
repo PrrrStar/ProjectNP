8480d4d81e1a370acfa71f29292f5a5eaa7a3c89
heosang <57525618+heosang@users.noreply.github.com>
2020-09-16 23:17:49 +0900

    ckeditor 추가

--- community/models.py
+++ community/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Post(models.Model):
     title           = models.CharField(max_length=50,verbose_name="제목")
-    content         = models.TextField(verbose_name="내용")
+    content         = RichTextField(verbose_name="내용")
     #author=models.ForeignKey()
     created_at      = models.DateTimeField(auto_now_add=True, verbose_name='등록날짜')
     modified_at     = models.DateTimeField(auto_now=True, verbose_name='수정날짜')
