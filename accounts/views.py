6367f470cb0832a3db2a333cdf4359487009b479
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-09-17 02:46:01 +0900

    모달 로그인 회원가입 띄우기

--- accounts/views.py
+++ accounts/views.py
@@ -4,20 +4,23 @@ from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
-
+from django.contrib.auth import authenticate
 
 #Session Create
 def login(request):
     if request.method == 'POST':
-        login_form = AuthenticationForm(request, request.POST)
-        if login_form.is_valid():
-            auth_login(request, login_form.get_user())
-        return redirect('/')
-    
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            auth_login(request, user)
+            return redirect('/')
+        else:
+            return redirect(request,'/', {'error':'돌아가. 당신은 내편이 아닙니다.'})
     else:
-        login_form = AuthenticationForm()
-    
-    return render(request, 'accounts/login.html', {'login_form' : login_form})
+        return redirect(request,'/')
 
 def logout(request):
     auth_logout(request)
