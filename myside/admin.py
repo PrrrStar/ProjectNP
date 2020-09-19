d94aa6755d0ee9e5727184cdaca30329a6a30276
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-09-19 19:59:57 +0900

    add Product Tag func

--- myside/admin.py
+++ myside/admin.py
@@ -16,7 +16,7 @@ class BrandAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name','category','img','price','stock','available_display','created_at','modified_at')
     prepopulated_fields = {'slug':('name',)}
-    filter_horizontal = ('brand',)
+    filter_horizontal = ('brand','tag',)
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
@@ -26,3 +26,6 @@ class CommentAdmin(admin.ModelAdmin):
 class ReplyAdmin(admin.ModelAdmin):
     list_display=('content',)
 
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name','created_at',)
