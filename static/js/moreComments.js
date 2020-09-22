3bcd6167be6a188d583404d11077521a3b520cbb
Seo Jun Hyeok <46551990+taulmax@users.noreply.github.com>
2020-09-22 16:39:33 +0900

    더보기 로직 수정
    더보기 로직 수정

--- static/js/moreComments.js
+++ static/js/moreComments.js
@@ -13,13 +13,15 @@ if (comments.length <= 3) {
 }
 
 function showMoreComments() {
-    for (i = startNum; i < startNum + 3; i++) {
-        comments[i].style.display = "flex";
-    }
-
-    startNum = startNum + 3;
-
-    if (startNum >= comments.length) {
+    if (comments.length - startNum > 3) {
+        for (i = startNum; i < startNum + 3; i++) {
+            comments[i].style.display = "flex";
+        }
+        startNum = startNum + 3;
+    } else {
+        for (i = startNum; i < comments.length; i++) {
+            comments[i].style.display = "flex";
+        }
         showMore.style.display = "none";
     }
 }
