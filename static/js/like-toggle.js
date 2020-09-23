4127f9731d07619a1f1034a8b311a8f40fd1360b
taulmax <46551990+taulmax@users.noreply.github.com>
2020-09-23 17:46:28 +0900

    내꺼하기 버튼 효과 구현
    내꺼하기 버튼 효과 구현

--- static/js/like-toggle.js
+++ static/js/like-toggle.js
@@ -1,27 +1,28 @@
-
-
-$("#btn-like").click(function(e){
-    e.preventDefault();
-    var likeCount =$('#like-count')
-    var likeURL = $(this).attr('data-url');
-    $.ajax({
-      url:likeURL,
-      method:"GET",
-      data:{},
-      success:function(data){
-        console.log(data);
-        var newLikes;
-        if (data.liked){
-            newLikes = parseInt(likeCount.text()) + 1
-            likeCount.text(newLikes)
-        }else{
-            newLikes = parseInt(likeCount.text()) -1
-            likeCount.text(newLikes)
-        }
-      }, error:function(error){
-        console.log(error);
-        console.log("error");
+$("#btn-like").click(function (e) {
+  e.preventDefault();
+  var likeCount = $("#like-count");
+  var likeURL = $(this).attr("data-url");
+  var likeBtn = $("#btn-like");
+  $.ajax({
+    url: likeURL,
+    method: "GET",
+    data: {},
+    success: function (data) {
+      console.log(data);
+      var newLikes;
+      if (data.liked) {
+        newLikes = parseInt(likeCount.text()) + 1;
+        likeCount.text(newLikes);
+        likeBtn.css("backgroundColor", "#fe4737");
+      } else {
+        newLikes = parseInt(likeCount.text()) - 1;
+        likeCount.text(newLikes);
+        likeBtn.css("backgroundColor", "#BBBBBB");
       }
-    })
-})
-
+    },
+    error: function (error) {
+      console.log(error);
+      console.log("error");
+    },
+  });
+});
