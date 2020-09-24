0defaa20121076d60bad652fc9465b1e0ec6fc7d
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-09-24 22:38:48 +0900

    deployMode RDS Conn

--- config/settings/deploy.py
+++ config/settings/deploy.py
@@ -1,17 +1,15 @@
 from .base import *
-import os
 
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
-
+ALLOWED_HOSTS = ['127.0.0.1','3.34.97.145']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'db-project-np.cuhffvh7u64m.ap-northeast-2.rds.amazonaws.com',
-        'NAME': 'project_NP',
-        'USER': 'NPadmin',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'database-np.cuhffvh7u64m.ap-northeast-2.rds.amazonaws.com',
+        'NAME': 'DatabaseNP',
+        'USER': 'myside',
         'PASSWORD': DATABASE_PASSWORD, 
         'PORT': '5432',
     }
@@ -26,16 +24,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+'''
 AWS_REGION = 'ap-northeast-2'
 AWS_STORAGE_BUCKET_NAME = 'projectnp-bucket'
 AWS_S3_CUSTOM_DOMAIN = f's3.{AWS_REGION}.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}'
@@ -52,3 +46,4 @@ FILE_UPLOAD_MAX_MEMORY_SIZE = 1024000000
 
 DEFAULT_FILE_STORAGE = 'config.storages.S3DefaultStorage'
 STATICFILES_STORAGE = 'config.storages.S3StaticStorage'
+'''
