2b79c9e1e96b97c7a88b615b74b447017bdf1d63
Seo Jun Hyeok <46551990+taulmax@users.noreply.github.com>
2020-09-27 07:59:36 +0900

    Update comment.js

--- static/js/comment.js
+++ static/js/comment.js
@@ -1,24 +1,26 @@
 $(function () {
-  $("#comment-form").submit(function (e) {
+  $("#btn-comment").click(function (e) {
     e.preventDefault();
     $("#comment-modal").modal("hide");
-    var url = $(this).attr("action");
-    var form = $(this).serialize();
+    var comment_form = $("#comment-form")[0];
+    var comment_formData = new FormData(comment_form);
+
+    var url = $("#comment-form").attr("action");
+    var form = $("#comment-form").serialize();
+
     $.ajax({
       url: url,
       enctype: "multipart/form-data",
       method: "POST",
-      data: form,
+      data: comment_formData,
+      async: true,
       processData: false,
       contentType: false,
       dataType: "json",
-      success: function(response){
-        $('#id_content').val("");
-        $("#id_img").val("");
-        $(".comment__comments").html(response['form']);
+      success: function (response) {
+        $(".comment__comments").html(response.form);
       },
-      
-      error: function(rs, e){
+      error: function (rs, e) {
         console.log(rs.responseText);
       },
     });
