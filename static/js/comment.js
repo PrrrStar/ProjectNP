85bbb41b032cd690d0a01a30ece1fc274695a5e2
taulmax <46551990+taulmax@users.noreply.github.com>
2020-09-26 16:46:53 +0900

    Update detail.html
    Update detail.html

--- static/js/comment.js
+++ static/js/comment.js
@@ -1,29 +1,28 @@
 $(function () {
-    $('#btn-comment').click(function(e){
-        e.preventDefault();
-        $('#comment-modal').modal('hide');
-        var comment_form = $('#comment-form')[0];
-        var comment_formData = new FormData(comment_form);
-        
-        var url = $('#comment-form').attr('action');
-        var form = $('#comment-form').serialize();
+  $("#btn-comment").click(function (e) {
+    e.preventDefault();
+    $("#comment-modal").modal("hide");
+    var comment_form = $("#comment-form")[0];
+    var comment_formData = new FormData(comment_form);
 
-        $.ajax({
-            url: url,
-            enctype: "multipart/form-data",
-            method: 'POST',
-            data: comment_formData,
-            async: true,
-            processData: false,
-            contentType: false,
-            dataType: 'json',
-            success: function(response){
-                $('.comment__main__comments').html(response.form);
-                
-            },
-            error: function(rs, e){
-                console.log(rs.responseText);
-            }
-        });
+    var url = $("#comment-form").attr("action");
+    var form = $("#comment-form").serialize();
+
+    $.ajax({
+      url: url,
+      enctype: "multipart/form-data",
+      method: "POST",
+      data: comment_formData,
+      async: true,
+      processData: false,
+      contentType: false,
+      dataType: "json",
+      success: function (response) {
+        $(".detail__comment").html(response.form);
+      },
+      error: function (rs, e) {
+        console.log(rs.responseText);
+      },
     });
-})
+  });
+});
