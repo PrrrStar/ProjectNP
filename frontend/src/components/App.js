b686e4056df4d73c0a96eeca3a1fbac8ad4afccf
Seo Jun Hyeok <46551990+taulmax@users.noreply.github.com>
2020-10-02 06:10:28 +0900

    리액트 드롭다운 메뉴 완성
    리액트 드롭다운 메뉴 완성

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Banner from './Banner';
 import Category from './Category';
+import '../css/reset.css';
 import '../css/base.css';
 
 const App = () => {
--- frontend/src/components/Category.js
+++ frontend/src/components/Category.js
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import '../css/category.css';
 
+
 const Category = () => {
   const [categories, setCategories] = useState([]);
+  const [drop, setDrop] = useState();
 
   useEffect(() => {
     getCategories();
@@ -14,16 +16,38 @@ const Category = () => {
     setCategories(data);
   }
 
+  const onClickCategory = event => {
+    const children = document.querySelectorAll(".category__children");
+    let name = event.target.getAttribute("value");
+    if (event.target.getAttribute("drop") == "f") {
+      for (var i = 0; i < children.length; i++) {
+        if (name == children[i].getAttribute("value")) {
+          children[i].style.display = "flex";
+        }
+        console.log(children[i]);
+      }
+      event.target.setAttribute("drop", "t")
+    }
+    else {
+      for (var j = 0; j < children.length; j++) {
+        if (name == children[j].getAttribute("value")) {
+          children[j].style.display = "none";
+        }
+      }
+      event.target.setAttribute("drop", "f")
+    }
+  }
+
   return (
     <>
       <div class="category">
         <div class="category__container">
           {categories.map(category => {
             if (category.parent) {
-              return <div class="category__children" value="{category.parent}"><a
+              return <div class="category__children" value={category.parent}><a
                 href="">{category.name}</a></div>
             } else {
-              return <div class="category__parent" value="{category.name}" drop="f">{category.name}</div>
+              return <div class="category__parent" value={category.id} drop="f" onClick={onClickCategory}>{category.name}</div>
             }
           })}
         </div>
