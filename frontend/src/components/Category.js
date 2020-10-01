15898517c7235783221a442710dd5b5fff1e2b8a
Seo Jun Hyeok <46551990+taulmax@users.noreply.github.com>
2020-10-02 05:48:08 +0900

    리액트 카테고리 css 추가
    리액트 카테고리 css 추가

--- frontend/src/components/Category.js
+++ frontend/src/components/Category.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
+import '../css/category.css';
 
 const Category = () => {
   const [categories, setCategories] = useState([]);
@@ -17,9 +18,14 @@ const Category = () => {
     <>
       <div class="category">
         <div class="category__container">
-          {categories.map(category => (
-            <div>{category.name}</div>
-          ))}
+          {categories.map(category => {
+            if (category.parent) {
+              return <div class="category__children" value="{category.parent}"><a
+                href="">{category.name}</a></div>
+            } else {
+              return <div class="category__parent" value="{category.name}" drop="f">{category.name}</div>
+            }
+          })}
         </div>
       </div>
     </>
