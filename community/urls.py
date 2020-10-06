38327339fb382828423f8a018b2671fb5ab79bd9
heosang <57525618+heosang@users.noreply.github.com>
2020-10-06 23:03:04 +0900

    rest framework 추가

--- community/urls.py
+++ community/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('<int:post_pk>/derecommend/', post_derecommend, name="post_derecommend"),
     path('<int:post_pk>/comment/', comment_create, name="comment_create"),
     path('<int:post_pk>/comment/<int:comment_pk>/delete', post_comment_delete, name="post_comment_delete"),
-
+    
+    path('api/posts-list', PostList.as_view(), name=PostList.name),
+    path('api/posts/<int:id>/', PostDetail.as_view(), name=PostDetail.name),
+    path('api/comments/', CommentList.as_view(),name=CommentList.name),
 ]
