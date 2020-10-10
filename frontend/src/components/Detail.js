2a10d0983d57bf1ac8fa7eb391586e7707b84f35
Seo Jun Hyeok <46551990+taulmax@users.noreply.github.com>
2020-10-11 04:24:14 +0900

    리액트 Comment 수정중
    리액트 Comment 수정중
    이거 왜캐 어렵냐 ㅠㅠㅠ

--- frontend/src/components/Detail.js
+++ frontend/src/components/Detail.js
@@ -2,6 +2,49 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import "../css/detail.css";
+import "../css/comment.css";
+
+const Comment = ({ comments }) => {
+  return (
+    <>
+      <span>{comments}</span>
+    </>
+  )
+}
+
+const CommentBox = ({ product }) => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    getComment();
+  }, [product])
+
+  const getComment = async () => {
+    if (product) {
+      const commentId = product.comments.map(comment => comment.split('/')[5]);
+      const commentArray = []
+      commentId.map(async id => {
+        const { data } = await axios.get(`/api/comments/${id}`);
+        commentArray.push(data);
+      });
+      setComments(commentArray);
+    };
+  };
+
+  return (
+    <div className="detail__comment">
+      <div className="comment__header">
+        <div className="comment__header--title">
+          <img src="/images/point.png" className="point" alt="point" />
+          상품후기({product ? product.comments.length : null}개)
+        </div>
+      </div>
+      <div className="comment__comments">
+        <Comment product={product} comments={comments} />
+      </div>
+    </div>
+  )
+}
 
 const DetailBox = ({ product }) => {
   return (
@@ -11,26 +54,26 @@ const DetailBox = ({ product }) => {
           {product ? product.category.first : null}
           <span> &gt; {product ? product.category.second : null}</span>
         </span>
-        <img class="detail__img" src={product ? product.img : null} alt="Product IMG" />
+        <img className="detail__img" src={product ? product.img : null} alt="Product IMG" />
       </div>
       <div className="detail__rightContent">
         <div className="detail__header">
-          <span class="detail__name">
-            <img src="/images/point.png" class="point" />
+          <span className="detail__name">
+            <img src="/images/point.png" className="point" alt="point" />
             {product ? product.name : null}
           </span>
-          <span class="detail__brand"></span>
+          <span className="detail__brand"></span>
         </div>
-        <div class="detail__price">
+        <div className="detail__price">
           <span>가격 {product ? product.price : null}원</span>
         </div>
-        <div class="detail__description">
-          <div class="description__content">
+        <div className="detail__description">
+          <div className="description__content">
             <span>상품설명</span>
             <span>{product ? product.description : null}</span>
           </div>
-          <div class="detail__tagBox">
-            <div class="detail__tag">
+          <div className="detail__tagBox">
+            <div className="detail__tag">
               <span>
                 <span>연관태그</span>
                 <form method="get" action="">
@@ -38,11 +81,21 @@ const DetailBox = ({ product }) => {
                 </form>
               </span>
             </div>
-            <div class="detail__tag--heart">
-              <i class="fas fa-heart"></i>
+            <div className="detail__tag--heart">
+              <i className="fas fa-heart"></i>
               <span id='like-count'>{product ? product.like.length : null}</span>
             </div>
           </div>
+        </div>
+        <div className="detail__button">
+          <button className="detail__stock">
+            <span>재고확인</span>
+            <img src="/images/location__white.png" alt="재고확인" />
+          </button>
+          <button type="button" className="detail__naeggu {% if user in product.like.all %}detail__naeggu__active{% endif %}" id='btn-like' data-url='{{ product.get_api_like_url }}' data-like='{{product.like.count}}'>
+            <span>내꺼하기</span>
+            <img src="/images/heart__white.png" alt="내꺼하기" />
+          </button>
         </div>
       </div>
     </div>
@@ -54,18 +107,18 @@ const Detail = () => {
   const [productDetail, setProductDetail] = useState();
 
   useEffect(() => {
-    getProduct();
-  }, []);
+    const getProduct = async () => {
+      const { data } = await axios.get("/api/products/" + slug);
+      setProductDetail(data);
+    };
 
-  const getProduct = async () => {
-    const { data } = await axios.get("/api/products/" + slug);
-    console.log(data.category.first);
-    setProductDetail(data);
-  };
+    getProduct();
+  }, [slug]);
 
   return (
     <main className="detail">
       <DetailBox product={productDetail} />
+      <CommentBox product={productDetail} />
     </main>
   );
 };
