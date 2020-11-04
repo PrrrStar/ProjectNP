44c2a3845ba55c446e4ef6e6181057780c00e662
heosang <57525618+heosang@users.noreply.github.com>
2020-11-05 00:41:13 +0900

    related_name 변경

--- community/models.py
+++ community/models.py
@@ -42,8 +42,8 @@ class Post(models.Model):
 
 
 class PostComment(models.Model):
-    post        = models.ForeignKey(Post, verbose_name="글", on_delete=models.CASCADE, related_name='comment')
-    author      = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comment')
+    post        = models.ForeignKey(Post, verbose_name="글", on_delete=models.CASCADE, related_name='comments')
+    author      = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
     content     = models.TextField(max_length = 100, verbose_name='내용')
     created_at  = models.DateTimeField(auto_now_add=True, verbose_name='등록날짜')
     modified_at = models.DateTimeField(auto_now = True, verbose_name='수정날짜')
@@ -52,4 +52,3 @@ class PostComment(models.Model):
         ordering            = ['-created_at']
         verbose_name        = 'comment'
         verbose_name_plural = 'comments'
-
