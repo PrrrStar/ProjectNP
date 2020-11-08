80823b545c3e8ef63de1b3176985818ee176733f
heosang <57525618+heosang@users.noreply.github.com>
2020-11-08 21:43:33 +0900

    mycvs  ==> user (변수명변경)

--- mycvs/admin.py
+++ mycvs/admin.py
@@ -4,7 +4,7 @@ from .models import *
 @admin.register(CVS)
 class CVSAdmin(admin.ModelAdmin):
     list_display=['name','latitude','longitude','brand','brand_logo','slug']
-    filter_horizontal = ('mycvs',)
+    filter_horizontal = ('user',)
     prepopulated_fields = {'slug':('name',)}
 
 @admin.register(Stock)
