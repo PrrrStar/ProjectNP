cb1ddc6bd4098e3970d91c59548c9bac2c812590
hyeonseong-design <70266152+hyeonseong-design@users.noreply.github.com>
2020-11-13 23:34:17 +0900

    Revert "Merge branch 'master' of https://github.com/PrrrStar/ProjectNP"
    This reverts commit b95db550d8864515568a05d7728eba055fc579ea, reversing
    changes made to c6fe030c60cfbef6cef1201c8c6a3f53d7f16f12.

--- mycvs/views.py
+++ mycvs/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.db.models import Count
-from django.views.decorators.http import require_http_methods
 import json
 from django.core.serializers.json import DjangoJSONEncoder
 from django.http import Http404, HttpResponseRedirect, HttpResponse, JsonResponse
@@ -9,8 +8,6 @@ from .models import *
 from accounts.models import User
 import decimal
 from django.core import serializers
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.text import slugify
 # Create your views here.
 def mymap(request):
 
@@ -31,60 +28,35 @@ def mymap(request):
 
     return render(request, 'mycvs/location.html', context)
 
-@csrf_exempt
-@require_http_methods(["GET", "POST"])
-def add_mycvs(request):
-    user = request.user  
-    postedPlace=json.loads(request.POST.get("place",""))
-    place_name=postedPlace['place_name']
-    postedCvs=CVS.objects.filter(name=place_name)
-    if(len(postedCvs)!=0): #try except로 바꿀 예정
-        postedCvs=CVS.objects.get(name=place_name)
-        postedCvs.user.add(user)
-    else:
-        mycvs=CVS()
-        mycvs.name=place_name
-        mycvs.slug=slugify(place_name, allow_unicode=True)
-        mycvs.latitude=postedPlace['x']
-        mycvs.longitude=postedPlace['y']
-        mycvs.brand, mycvs.brand_logo=find_brand(place_name)
-        mycvs.save()
-        mycvs.user.add(user)
-    message = "좋아요"
-    context = {'message': message }
-    return HttpResponse(json.dumps(context), content_type="application/json")  
-
-def find_brand( name):
-    if "이마트24" in name:
-        return "이마트24", "brand/이마트24.jpg"
-    elif "CU" in name:
-        return "CU", "brand/씨유.jpg"
-    elif "세븐일레븐" in name:
-        return "세븐일레븐", "brand/세븐일레븐.jpg"
-    elif "스토리웨이" in name:
-        return "스토리웨이", "brand/스토리웨이.jpg"
-    elif "GS25" in name:
-        return "GS25", "brand/GS25.jpg"
-    elif "미니스톱" in name:
-        return "미니스톱", "brand/미니스톱.jpg"
-    elif "IGA" in name:
-        return "IGA", "brand/IGA.jpg"
-    elif "로그인" in name:
-        return "로그인", "brand/로그인.jpg"
-    elif "씨스페이스" in name:
-        return "씨스페이스", "brand/씨스페이스.jpg" 
-    else :
-        return "기타"  
-
-@csrf_exempt
-@require_http_methods(["GET", "POST"])
-def delete_mycvs(request):
-    user = request.user  
-    postedPlace=json.loads(request.POST.get("place",""))
-    postedCvs=CVS.objects.get(name=postedPlace)
-    postedCvs.user.remove(user)
-    if (postedCvs.user.count()==0):
-        postedCvs.delete()
-    message = "좋아요 취소"
-    context = {'message': message }
-    return HttpResponse(json.dumps(context), content_type="application/json")  
+# def myCvs_create(request):
+#     user = request.user
+#     if request.method =='POST':
+#         asdf=CVS()
+#         asdf.user=user
+#         asdf.longitude=y
+#         asdf.latitude=x
+#         asdf.name=name
+#         asdf.brand, asdf.brand_logo=find_brand(name)
+#         asdf.save()
+#         return redirect('location.html')
+#     def find_name(name):
+#         if "이마트24" in name:
+#             return "이마트24", "media/brand/이마트24.jpg"
+#         elif "CU" in name:
+#             return "CU", "media/brand/씨유.jpg"
+#         elif "세븐일레븐" in name:
+#             return "세븐일레븐", "media/brand/세븐일레븐.jpg"
+#         elif "스토리웨이" in name:
+#             return "스토리웨이", "media/brand/스토리웨이.jpg"
+#         elif "GS25" in name:
+#             return "GS25", "media/brand/GS25.jpg"
+#         elif "미니스톱" in name:
+#             return "미니스톱", "media/brand/미니스톱.jpg"
+#         elif "IGA" in name:
+#             return "IGA", "media/brand/IGA.jpg"
+#         elif "로그인" in name:
+#             return "로그인", "media/brand/로그인.jpg"
+#         elif "씨스페이스" in name:
+#             return "씨스페이스", "media/brand/씨스페이스.jpg" 
+#         else :
+#             return "기타"            
